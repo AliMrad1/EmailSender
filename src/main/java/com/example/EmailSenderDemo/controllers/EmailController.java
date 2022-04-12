c7bf09fdaed8865ce303b546848358f271b4d1d4
Ali Mrad <am8396280@gmail.com>
2022-04-12 20:57:21 +0300

    update

--- src/main/java/com/example/EmailSenderDemo/controllers/EmailController.java
+++ src/main/java/com/example/EmailSenderDemo/controllers/EmailController.java
@@ -10,7 +10,6 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/email")
 @AllArgsConstructor
-@CrossOrigin("*")     // use for testing code with multiple host, but not recommended
 public class EmailController {
 
     private final EmailService emailService;
