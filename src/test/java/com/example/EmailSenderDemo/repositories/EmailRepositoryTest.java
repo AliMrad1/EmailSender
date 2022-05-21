0472eead55ccd2abce26ed7bbd8b48a85aa37bb8
Ali Mrad <am8396280@gmail.com>
2022-05-21 19:14:53 +0300

    Testing Repository Layer

---
+++ src/test/java/com/example/EmailSenderDemo/repositories/EmailRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.EmailSenderDemo.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailRepositoryTest {
+
+    @Test
+    void findSameEmail() {
+    }
+
+    @Test
+    void getAllEmails() {
+    }
+}
